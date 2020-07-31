f5fd09cc9732b3a2f651a12b3c9df565b1165f51
boy88 <3193502@qq.com>
2020-07-31 18:50:18 +0800

    second time to read

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8-*-
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
+@app.route('/home')
 def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+    return '<h1>Hello 恒升指数Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
